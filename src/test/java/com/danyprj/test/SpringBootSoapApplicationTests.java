4b91d487f7a24bcf247f3a609bc8498d085a3ba8
Dany Samuel <danysamuel@gmail.com>
2018-02-08 01:30:49 +1030

    first commit

--- src/test/java/com/danyprj/test/SpringBootSoapApplicationTests.java
+++ src/test/java/com/danyprj/test/SpringBootSoapApplicationTests.java
@@ -1,4 +1,4 @@
-package com.innova;
+package com.danyprj.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +11,7 @@ public class SpringBootSoapApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
 	}
 
 }
